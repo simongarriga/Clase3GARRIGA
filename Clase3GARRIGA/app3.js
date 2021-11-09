08d2f4ddf28d0221626ff402f345943af9943488
Simon <83841211+simongarriga@users.noreply.github.com>
2021-11-09 18:40:31 -0300

    Add files via upload

---
+++ Clase3GARRIGA/app3.js
@@ -0,0 +1,38 @@
+// EJEMPLO 1
+
+// for (let i = 0; i <= 9; i++) {
+//     console.log('HOLA CODER');
+// }
+
+// EJEMPLO 2
+
+let preguntaNumero = prompt('Cual es mi numero favorito?');
+let error = 0;
+
+while(preguntaNumero != 7) {
+    preguntaNumero = prompt('Ese no es!, PISTA es del 1 al 10!');
+    error++;
+
+}
+
+
+    alert('Felicitaciones el 7 es mi favorito!')
+
+    alert(`Lo intentaste en ${error} oportunidades! `);
+
+
+
+
+
+// EJEMPLO 3
+
+// let numeroZ = parseInt(prompt('Pon un numero'));
+
+// for(i = 1; i <=10; i++) {
+    
+//     let resultado = numeroZ + i;
+    
+//     alert(numeroZ + '+' + i + '=' + resultado);
+// }
+
+    
